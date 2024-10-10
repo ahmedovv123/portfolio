b15e78dc5a624bd6954baeef7667d6ba1ef84d2e
Ahmet Ahmedov <ahmet@hph.io>
2024-10-10 16:43:24 +0300

    center image

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,7 +6,7 @@ import authorImage from "@/public/images/authors/IMG_2928.JPG"
 
 export default function Intro() {
   return (
-    <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
+    <section className='flex flex-col-reverse items-center gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
         <h1 className='title no-underline'>Hey, I&#39;m Ahmet.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
