25ee75e3b5c9881895945d0c917fea475a4c620a
Ahmet Ahmedov <ahmet@hph.io>
2024-10-10 13:30:17 +0300

    fix build errors

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { JSX } from 'react'
 import { highlight } from 'sugar-high'
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
@@ -5,7 +7,7 @@ import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
 import Counter from '@/components/counter'
 
 function Code({ children, ...props }: any) {
-  let codeHTML = highlight(children)
+  const codeHTML = highlight(children)
   return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
 }
 
